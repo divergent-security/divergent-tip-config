2db256bdf418d88022ee0d1270307c82f8506976
cneckar <cneckar@divergentsecurity.com>
2019-04-27 10:33:35 -0600

    make the import explicit

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
 # THis file and constants.py will go away once we convert everything over to jsonschema
 
-from constants import *
+from constants import supported_services, \
+                      loggable_events, \
+                      priority_strings, \
+                      discovery_mechanisms, \
+                      job_input_classes, \
+                      job_output_classes, \
+                      protocols
